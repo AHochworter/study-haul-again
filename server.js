51f3d6d12552f1792091ac782762eaedfd7e7133
Adrian Zabolitzki <adrianzab@gmail.com>
2023-10-27 12:04:53 -0600

    Add the resoponse functionality for specific question with an id

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Study-Haul';
+
+app.use(express.json());
+
 app.locals.questions = [
   {
     id: 1,
@@ -27,17 +32,36 @@ app.locals.questions = [
 
 
 
-app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Study-Haul';
 
 app.get('/api/v1/questions', (request, response) => {
   const questions = app.locals.questions;
   response.json({questions})
 })
 
+app.get('/api/v1/questions/:id', (request, response) => {
+  const id = request.params.id
+  // const questions = app.locals.questions;
+ 
+  console.log('paramsID: ', id)
+  // console.log('questions: ', questions)
+  
+  const foundQuestion = app.locals.questions.find((question) => 
+    question.id === id
+  );
+  if (foundQuestion) {
+    console.log('foundQuestion: ', foundQuestion)
+    response.status(200).json(foundQuestion)
+  } else {
+    return response.status(404).send('Page not found.')
+  }
+  });
+
+
 app.get('/', (request, response) => {
   response.send('Oh hey Study Haul');
 });
+
+
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
