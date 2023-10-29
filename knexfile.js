768c9c3e3515c4b8a5dee62aaf2bcdc12f807680
Jan McSorley <mcsorleyjan@gmail.com>
2023-10-29 15:01:34 -0500

    Setup Knex

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './dev.sqlite3'
+  //   }
+  // },
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'study_haul',
+      user:     'janmcsorley',
+      password: 'null'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
